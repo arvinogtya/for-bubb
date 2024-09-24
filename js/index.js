6c25751e4d8404f6f6c16c121bc89b425853c4ce
arvinogtya <168400720+arvinogtya@users.noreply.github.com>
2024-09-24 20:58:14 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Bubb UDH EFFORTS BELUM NIII'.split('')
+const text = 'Bubb UDH EFFORTS BELUM NIII😛'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
